cf96857d4136805121dc2f4c7ed9bbfd268d3baa
YITSUKEN <gametopsss@gmail.com>
2021-02-28 22:52:50 +0300

    list to class fix

--- map.py
+++ map.py
@@ -2,12 +2,15 @@ import generator
 import math
 class city:
     def __init__(self, seed, x, y):
+        self.seed = seed
         self.x = x
         self.y = y
-class farms:
+class farm:
     def __init__(self, seed, x, y, type):
+        self.seed = seed
         self.x = x
         self.y = y
+        self.type = type
 class country:
     def __init__(self, seed):
         self.seed = seed
@@ -21,11 +24,12 @@ class country:
             y = y + 1
             is_ok = True
             for i in range(len(self.cities)):
-                if abs(generator.get_number(seed, x, 0, 1000) - self.cities[i][0]) < 200 and abs(generator.get_number(seed, y, 0, 1000) - self.cities[i][1]) < 200:
+                if abs(generator.get_number(seed, x, 0, 1000) - self.cities[i].x) < 200 and abs(generator.get_number(seed, y, 0, 1000) - self.cities[i].y) < 200:
                     is_ok = False
                     break
             if is_ok:
-                self.cities.append(city(seed, round(generator.get_number(seed, x, 0, 1000)), round(generator.get_number(seed, y, 0, 1000))))
+                c = city(seed, round(generator.get_number(seed, x, 0, 1000)), round(generator.get_number(seed, y, 0, 1000)))
+                self.cities.append(c)
             if len(self.cities) == 5:
                 break
         x = 745
@@ -38,14 +42,15 @@ class country:
             z = z + 3
             is_ok = True
             for i in range(len(self.cities)):
-                if abs(generator.get_number(seed, x, 0, 1000) - self.cities[i][0]) < 50 and abs(generator.get_number(seed, y, 0, 1000) - self.cities[i][1]) < 50:
+                if abs(generator.get_number(seed, x, 0, 1000) - self.cities[i].x) < 50 and abs(generator.get_number(seed, y, 0, 1000) - self.cities[i].y) < 50:
                     is_ok = False
                     break
             for i in range(len(self.farms)):
-                if abs(generator.get_number(seed, x, 0, 1000) - self.farms[i][0]) < 50 and abs(generator.get_number(seed, y, 0, 1000) - self.farms[i][1]) < 50:
+                if abs(generator.get_number(seed, x, 0, 1000) - self.farms[i].x) < 50 and abs(generator.get_number(seed, y, 0, 1000) - self.farms[i].y) < 50:
                     is_ok = False
                     break
             if is_ok:
-                self.farms.append([round(generator.get_number(seed, x, 0, 1000)), round(generator.get_number(seed, y, 0, 1000)), farms[round(generator.get_number(seed, y, 0, len(farms)-1))]])
+                f = farm(seed, round(generator.get_number(seed, x, 0, 1000)), round(generator.get_number(seed, y, 0, 1000)), farms[round(generator.get_number(seed, y, 0, len(farms)-1))])
+                self.farms.append(f)
             if len(self.farms) == 10:
                 break
